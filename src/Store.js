d561b108f8e923b4d0f7d53c946ee72e361a5b00
The Mai <Mai.The.09@gmail.com>
2019-05-10 17:00:21 -0500

    progress

---
+++ src/Store.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export const Store = React.createContext();
+
+const initialState = {
+  episodes: [],
+  favorites: [],
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_DATA': return { ...state, episodes: action.payload };
+    default: return state;
+  }
+}
+
+export function StoreProvider(props) {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  // The above line is the same as:
+  // const value = {
+  //   state: state,
+  //   dispatch: dispatch
+  // }
+
+  return <Store.Provider value={value}>
+    {props.children}
+  </Store.Provider>
+}
